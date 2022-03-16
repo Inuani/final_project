d6e0035ab9ff656bbc65c9d8e977973780b03575
LX <hello@gmail.com>
2022-03-16 12:33:31 +0100

    first_commit

---
+++ src/minter_assets/src/index.js
@@ -0,0 +1,65 @@
+// The minter is the representation of the minter contract in main.mo but in JavaScript
+import { minter } from "../../declarations/minter";
+
+// This is library to use with principal that is provided by Dfinity
+import { Principal } from "@dfinity/principal";
+
+// For beginners : This is really basic Javascript code that add an event to the "Mint" button so that the mint_nft function is called when the button is clicked.
+const mint_button = document.getElementById("mint");
+mint_button.addEventListener("click", mint_nft);
+
+async function mint_nft() {
+  // Get the url of the image from the input field
+  const name = document.getElementById("name").value.toString();
+  console.log("The url we are trying to mint is " + name);
+
+  // Get the principal from the input field.
+  const principal_string = document.getElementById("principal").value.toString();
+  const principal = Principal.fromText(principal_string);
+
+  // Mint the image by calling the mint_principal function of the minter.
+  const mintId = await minter.mint_principal(name, principal);
+  console.log("The id is " + Number(mintId));
+  // Get the id of the minted image.
+
+  // Get the url by asking the minter contract.
+  document.getElementById("nft").src = await minter.tokenURI(mintId);
+
+  // Show some information about the minted image.
+  document.getElementById("greeting").innerText = "this nft owner is " + principal_string + "\nthis token id is " + Number(mintId);
+}
+
+const plugLogin = document.getElementById("plugLogin");
+const plugLogout = document.getElementById("plugLogout");
+
+let connected = false;
+
+(async () => {
+  connected = await window.ic.plug.isConnected();
+  if (connected) {
+    console.log('logged');
+    const requestBalanceResponse = await window.ic?.plug?.requestBalance();
+    const balance = requestBalanceResponse[0]?.value;
+    console.log(balance);
+
+    plugLogin.style.display = 'none';
+  } else {
+    plugLogout.style.display = 'none';
+  }
+})()
+
+
+plugLogin.addEventListener("click", () => {
+  (async () => {
+
+    // Canister Ids
+    const nnsCanisterId = 'ryjl3-tyaaa-aaaaa-aaaba-cai'
+
+    // Whitelist
+    const whitelist = [
+      nnsCanisterId,
+    ];
+    const connected = await window.ic.plug.isConnected();
+    if (!connected) await window.ic.plug.requestConnect({ whitelist });
+  })();
+});
